e24610eb2fba9c3f7c625963dead6c3c5b0563b4
Vivian Velazquez <vivianmvero@gmail.com>
2025-04-23 22:15:04 -0600

    Documento final con pruebas

--- BONZAI_VistasMaterializadas&Desnormalizacion.sql
+++ BONZAI_VistasMaterializadas&Desnormalizacion.sql
@@ -25,18 +25,18 @@ AS
 SELECT
     c.id_cliente,
     c.nombre AS NombreCliente,
-    c.apellidos AS ApellidosCliente,
+    c.apellido AS ApellidosCliente,
     COUNT(ci.id_cita) AS TotalCitas
 FROM
-    CLIENTES c
+    CLIENTE c
 JOIN
     CITAS ci ON c.id_cliente = ci.id_cliente
 GROUP BY
-    c.id_cliente, c.nombre, c.apellidos;
-
+    c.id_cliente, c.nombre, c.apellido;
+/
 -- Consultar vista materializada
 SELECT * FROM CONTEO_CITAS_CLIENTE_MV;
-
+/
 
 -- 2. Ingresos por terapeuta
 CREATE MATERIALIZED VIEW INGRESOS_POR_TERAPEUTA_MV
@@ -46,19 +46,19 @@ AS
 SELECT
     t.id_terapeuta,
     t.nombre AS NombrTerapeuta,
-    t.apellidos AS ApellidosTerapeuta,
+    t.apellido AS ApellidosTerapeuta,
     SUM(p.monto) AS IngresosTotales
 FROM
-    TERAPEUTAS t
+    TERAPEUTA t
 JOIN
     CITAS c ON t.id_terapeuta = c.id_terapeuta
 JOIN
     PAGOS p ON c.id_cliente = p.id_cliente
 GROUP BY
-    t.id_terapeuta, t.nombre, t.apellidos;
+    t.id_terapeuta, t.nombre, t.apellido;
 
 -- Consultar vista materializada
-SELECT * FROM INGRESOS_POR_TERAPEUTA_MV;
+SELECT * FROM system.INGRESOS_POR_TERAPEUTA_MV;
 
 
 -- 3. Resumen de expedientes por cliente
@@ -69,17 +69,17 @@ AS
 SELECT
     c.id_cliente,
     c.nombre AS NombreCliente,
-    c.apellidos AS ApellidosCliente,
+    c.apellido AS ApellidosCliente,
     COUNT(e.id_expediente) AS TotalExpedientes
 FROM
-    CLIENTES c
+    CLIENTE c
 LEFT JOIN
     EXPEDIENTES e ON c.id_cliente = e.id_cliente
 GROUP BY
-    c.id_cliente, c.nombre, c.apellidos;
+    c.id_cliente, c.nombre, c.apellido;
 
 -- Consultar vista materializada
-SELECT * FROM RESUMEN_EXPEDIENTES_CLIENTE_MV;
+SELECT * FROM system.RESUMEN_EXPEDIENTES_CLIENTE_MV;
 
 
 
@@ -91,17 +91,17 @@ ALTER TABLE PAGOS
 ADD (nombre_cliente VARCHAR2(100), apellidos_cliente VARCHAR2(100));
 
 /* Se necesitará un trigger para mantener las columnas actualizadas cuando se 
-insertan nuevos pagos o se modifican los clientes. */
+insertan nuevos pagos o se modifican los cliente. */
 CREATE OR REPLACE TRIGGER PAGOS_ACTUALIZAR_CLIENTE_TRG
 BEFORE INSERT OR UPDATE ON PAGOS
 FOR EACH ROW
 DECLARE
-    v_nombre_cliente CLIENTES.nombre%TYPE;
-    v_apellidos_cliente CLIENTES.apellidos%TYPE;
+    v_nombre_cliente cliente.nombre%TYPE;
+    v_apellidos_cliente cliente.apellido%TYPE;
 BEGIN
-    SELECT nombre, apellidos
+    SELECT nombre, apellido
     INTO v_nombre_cliente, v_apellidos_cliente
-    FROM CLIENTES
+    FROM CLIENTE
     WHERE id_cliente = :NEW.id_cliente;
 
     :NEW.nombre_cliente := v_nombre_cliente;
@@ -118,12 +118,12 @@ CREATE OR REPLACE TRIGGER CITAS_ACTUALIZAR_TERAPEUTA_TRG
 BEFORE INSERT OR UPDATE ON CITAS
 FOR EACH ROW
 DECLARE
-    v_nombre_terapeuta TERAPEUTAS.nombre%TYPE;
-    v_apellidos_terapeuta TERAPEUTAS.apellidos%TYPE;
+    v_nombre_terapeuta terapeuta.nombre%TYPE;
+    v_apellidos_terapeuta terapeuta.apellido%TYPE;
 BEGIN
-    SELECT nombre, apellidos
+    SELECT nombre, apellido
     INTO v_nombre_terapeuta, v_apellidos_terapeuta
-    FROM TERAPEUTAS
+    FROM TERAPEUTA
     WHERE id_terapeuta = :NEW.id_terapeuta;
 
     :NEW.nombre_terapeuta := v_nombre_terapeuta;
